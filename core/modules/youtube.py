b3d50f893aa4d4a8ab768b0ff89715990a81a31b
PrzemyslawSarnacki <przemos100@gmail.com>
2020-04-18 18:46:48 +0200

    Compressing full playlist to zip file

--- core/modules/youtube.py
+++ core/modules/youtube.py
@@ -1,5 +1,6 @@
 import youtube_dl
 import os
+import zipfile
 
 
 def my_hook(d):
@@ -43,19 +44,21 @@ def download_mp3(link):
     return filename
 
 def download_playlist(link):
+    dir_name = r"C:/Users/Przemyslaw/Projects/ToolsSite/core/downloads/"
     ydl_opts = {
         "outtmpl": "/core/downloads/%(title)s.%(ext)s",
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(link, download=False)
+        info = ydl.extract_info(link, download=True)
         filenames = []
         for video in info['entries']:
             if not video:
                 print('ERROR: Unable to get info. Continuing...')
                 continue
-            filenames.append(f"{video.get('title')}.{video.get('ext')}")
-    print(filenames)
-    return filenames
-
-
-download_playlist("https://www.youtube.com/playlist?list=PLVArNR3Nc3LgUTNewtI-OY08HXJq_6Xy_")
+            title = video.get('title').replace(":", " -")
+            filenames.append(f"{title}.{video.get('ext')}")
+    zip_file = zipfile.ZipFile(f"{dir_name}down.zip", 'w')
+    with zip_file:
+        for filename in filenames:
+            zip_file.write(f"{dir_name}{filename}", arcname=filename)
+    return "down.zip"
